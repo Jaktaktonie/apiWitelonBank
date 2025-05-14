880c8415df7cf3145de899bbd10624795d83b885
andrzej <andrzej.czabajski@collegiumwitelona.pl>
2025-05-14 16:41:19 +0200

    zmiany api uzytkownik

--- app/Models/Uzytkownik.php
+++ app/Models/Uzytkownik.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Uzytkownik extends Model
+class Uzytkownik extends Authenticatable
 {
-    //
+    use HasFactory;
+
+    protected $table = 'uzytkownicy';
+
+    protected $fillable = [
+        'imie',
+        'nazwisko',
+        'email',
+        'telefon',
+        'weryfikacja',
+        'administrator',
+        'haslo_hash',
+    ];
+
+    protected $hidden = [
+        'haslo_hash',
+        'remember_token',
+    ];
+
+    public $timestamps = true;
 }
